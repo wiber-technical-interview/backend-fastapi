8c4551e281e16af80a1f2eb652252f14de6f46eb
MGConil <germanconil@hotmail.com>
2023-10-15 20:38:27 -0300

    add var mongodb raliway

--- config/db.py
+++ config/db.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
 
+mongo_url = "mongodb://mongo:eGGE5a+G2FhD3dbHg2Eg1CCg3EC36DEH@viaduct.proxy.rlwy.net:11089"
 
 # Configurar la conexión a MongoDB
-connection = MongoClient("mongodb://database:27017/") # conectar a la base de datos
+connection = MongoClient(mongo_url)#"mongodb://database:27017/" # conectar a la base de datos
 #Crear base de dato 
 db = connection["DB_Scripts"]
 #Crear coleccion 
