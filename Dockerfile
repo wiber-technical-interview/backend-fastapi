978c7338326238dc58c0b7c02972e0d06090dd46
MGConil <germanconil@hotmail.com>
2023-10-15 19:58:28 -0300

    change name file requirements

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.8-slim
 WORKDIR /app
 COPY requeriments*.txt ./
-RUN pip install --upgrade -r requeriments.txt
+RUN pip install --upgrade -r requirements.txt
 COPY . .
 EXPOSE 8000
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 
 # Configurar la conexión a MongoDB
-connection = MongoClient("mongodb://localhost:27017/") # conectar a la base de datos
+connection = MongoClient("mongodb://database:27017/") # conectar a la base de datos
 #Crear base de dato 
 db = connection["DB_Scripts"]
 #Crear coleccion 
